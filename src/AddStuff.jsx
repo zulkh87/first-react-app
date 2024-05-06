297f8ad4222cf0edfcf0ecd6bc52dca6ff315120
Zulkh87-AF-K <156850017+zulkh87@users.noreply.github.com>
2024-05-06 16:36:28 +0300

    add static component

---
+++ src/AddStuff.jsx
@@ -0,0 +1,9 @@
+export default function Avatar(){
+    return (
+        <img
+        className="avatar"
+        src="https://i.imgur.com/7vQD0fPs.jpg"
+        alt="Gregorio Y. Zara"
+        />
+    )
+}
