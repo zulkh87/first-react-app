0630f8073433660f57a805cdb1d53eae514f6107
Zulkh87-AF-K <156850017+zulkh87@users.noreply.github.com>
2024-05-06 13:14:19 +0300

    Added my React Project

---
+++ src/Greeting.jsx
@@ -0,0 +1,7 @@
+function Greeting() {
+    return <h1>&quot;Paintings are a form of art.&quot;</h1>;
+  }
+  
+  export default Greeting;
+
+  // React components must be capitalized or they will not function as expected, which is why we capitalized Greeting()
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+import Greeting from "./Greeting.jsx"
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Greeting />
+  </React.StrictMode>,
+)
+
+// React components must be capitalized or they will not function as expected, which is why we capitalized Greeting()
